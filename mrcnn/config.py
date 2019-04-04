93af6b888270dd34ff32a6b36c8aca563c09b8ae
Rohan Pramod Prabhu <prabhu4@ee220clnx1.ecn.purdue.edu>
2019-04-04 18:47:42 -0400

    tabbed config file

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -13,7 +13,7 @@ import numpy as np
 # Base Configuration Class
 # Don't use this class directly. Instead, sub-class it and override
 # the configurations you need to change.
-
+	
 class Config(object):
     """Base configuration class. For custom configurations, create a
     sub-class that inherits from this one and override properties
@@ -23,16 +23,16 @@ class Config(object):
     # Useful if your code needs to do things differently depending on which
     # experiment is running.
     NAME = None  # Override in sub-classes
-
+    
     # NUMBER OF GPUs to use. When using only a CPU, this needs to be set to 1.
     GPU_COUNT = 1
-
+    
     # Number of images to train with on each GPU. A 12GB GPU can typically
     # handle 2 images of 1024x1024px.
     # Adjust based on your GPU memory and image sizes. Use the highest
     # number that your GPU can handle for best performance.
     IMAGES_PER_GPU = 2
-
+    
     # Number of training steps per epoch
     # This doesn't need to match the size of the training set. Tensorboard
     # updates are saved at the end of each epoch, so setting this to a
@@ -41,12 +41,12 @@ class Config(object):
     # might take a while, so don't set this too small to avoid spending
     # a lot of time on validation stats.
     STEPS_PER_EPOCH = 1000
-
+    
     # Number of validation steps to run at the end of every training epoch.
     # A bigger number improves accuracy of validation stats, but slows
     # down the training.
     VALIDATION_STEPS = 50
-
+    
     # Backbone network architecture
     # Supported values are: resnet50, resnet101.
     # You can also provide a callable that should have the signature
@@ -71,14 +71,14 @@ class Config(object):
 
     # Number of classification classes (including background)
     NUM_CLASSES = 1  # Override in sub-classes
-
+    
     # Length of square anchor side in pixels
     RPN_ANCHOR_SCALES = (32, 64, 128, 256, 512)
 
     # Ratios of anchors at each cell (width/height)
     # A value of 1 represents a square anchor, and 0.5 is a wide anchor
     RPN_ANCHOR_RATIOS = [0.5, 1, 2]
-
+    
     # Anchor stride
     # If 1 then anchors are created for each cell in the backbone feature map.
     # If 2, then anchors are created for every other cell, and so on.
@@ -87,7 +87,7 @@ class Config(object):
     # Non-max suppression threshold to filter RPN proposals.
     # You can increase this during training to generate more propsals.
     RPN_NMS_THRESHOLD = 0.7
-
+    
     # How many anchors per image to use for RPN training
     RPN_TRAIN_ANCHORS_PER_IMAGE = 256
     
@@ -138,7 +138,7 @@ class Config(object):
 
     # Image mean (RGB)
     MEAN_PIXEL = np.array([123.7, 116.8, 103.9])
-
+    
     # Number of ROIs per image to feed to classifier/mask heads
     # The Mask RCNN paper uses 512 but often the RPN doesn't generate
     # enough positive proposals to fill this and keep a positive:negative
@@ -148,7 +148,7 @@ class Config(object):
 
     # Percent of positive ROIs used to train classifier/mask heads
     ROI_POSITIVE_RATIO = 0.33
-
+    
     # Pooled ROIs
     POOL_SIZE = 7
     MASK_POOL_SIZE = 14
@@ -156,34 +156,34 @@ class Config(object):
     # Shape of output mask
     # To change this you also need to change the neural network mask branch
     MASK_SHAPE = [28, 28]
-
+    
     # Maximum number of ground truth instances to use in one image
     MAX_GT_INSTANCES = 100
-
+    
     # Bounding box refinement standard deviation for RPN and final detections.
     RPN_BBOX_STD_DEV = np.array([0.1, 0.1, 0.2, 0.2])
     BBOX_STD_DEV = np.array([0.1, 0.1, 0.2, 0.2])
-
+    
     # Max number of final detections
     DETECTION_MAX_INSTANCES = 100
-
+    
     # Minimum probability value to accept a detected instance
     # ROIs below this threshold are skipped
     DETECTION_MIN_CONFIDENCE = 0.7
-
+    
     # Non-maximum suppression threshold for detection
     DETECTION_NMS_THRESHOLD = 0.3
-
+    
     # Learning rate and momentum
     # The Mask RCNN paper uses lr=0.02, but on TensorFlow it causes
     # weights to explode. Likely due to differences in optimizer
     # implementation.
     LEARNING_RATE = 0.001
     LEARNING_MOMENTUM = 0.9
-
+    
     # Weight decay regularization
     WEIGHT_DECAY = 0.0001
-
+    
     # Loss weights for more precise optimization.
     # Can be used for R-CNN training setup.
     LOSS_WEIGHTS = {
@@ -209,28 +209,28 @@ class Config(object):
 
     # Gradient norm clipping
     GRADIENT_CLIP_NORM = 5.0
-#nvidia-smi -q
-def __init__(self):
-    """Set values of computed attributes."""
+    #nvidia-smi -q
+    def __init__(self):
+    #"""Set values of computed attributes."""
     # Effective batch size
-    self.BATCH_SIZE = self.IMAGES_PER_GPU * self.GPU_COUNT
+        self.BATCH_SIZE = self.IMAGES_PER_GPU * self.GPU_COUNT
 
     # Input image size
-    if self.IMAGE_RESIZE_MODE == "crop":
-        self.IMAGE_SHAPE = np.array([self.IMAGE_MIN_DIM, self.IMAGE_MIN_DIM,
-            self.IMAGE_CHANNEL_COUNT])
-    else:
-        self.IMAGE_SHAPE = np.array([self.IMAGE_MAX_DIM, self.IMAGE_MAX_DIM,
-            self.IMAGE_CHANNEL_COUNT])
+        if self.IMAGE_RESIZE_MODE == "crop":
+                    self.IMAGE_SHAPE = np.array([self.IMAGE_MIN_DIM, self.IMAGE_MIN_DIM,
+                    self.IMAGE_CHANNEL_COUNT])
+        else:
+                    self.IMAGE_SHAPE = np.array([self.IMAGE_MAX_DIM, self.IMAGE_MAX_DIM,
+                    self.IMAGE_CHANNEL_COUNT])
 
     # Image meta data length
     # See compose_image_meta() for details
-    self.IMAGE_META_SIZE = 1 + 3 + 3 + 4 + 1 + self.NUM_CLASSES
+        self.IMAGE_META_SIZE = 1 + 3 + 3 + 4 + 1 + self.NUM_CLASSES
 
-def display(self):
-    """Display Configuration values."""
-    print("\nConfigurations:")
-    for a in dir(self):
-        if not a.startswith("__") and not callable(getattr(self, a)):
-            print("{:30} {}".format(a, getattr(self, a)))
-    print("\n")
+    def display(self):
+    #"""Display Configuration values."""
+        print("\nConfigurations:")
+        for a in dir(self):
+            if not a.startswith("__") and not callable(getattr(self, a)):
+                print("{:30} {}".format(a, getattr(self, a)))
+        print("\n")
