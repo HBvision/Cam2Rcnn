163776c6b3ea99559139f052542ee8dcc072df1d
ethanglaser <glasere@purdue.edu>
2019-04-17 11:45:14 -0400

    writing to file

--- samples/demo.py
+++ samples/demo.py
@@ -1,25 +1,23 @@
-import numpy as np
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mask R-CNN Demo
+# 
+# A quick intro to using the pre-trained model to detect and segment objects.
+
+# In[1]:
+
+
 import os
-import six.moves.urllib as urllib
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import sys
-import tarfile
-import tensorflow as tf
-import zipfile
-import cv2
-
-from collections import defaultdict
-from io import StringIO
-import matplotlib
-from matplotlib import pyplot as plt
-from PIL import Image
-
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-#from object_detection.utils import ops as utils_ops
-
 import random
 import math
+import numpy as np
 import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+import time
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
@@ -33,9 +31,7 @@ from mrcnn import visualize
 sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
 import coco
 
-#%matplotlib inline 
-
-image_name = 'OriginalM.jpg'
+#matplotlib inline 
 
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
@@ -47,16 +43,31 @@ if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
 
 # Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+IMAGE_DIR = os.path.join(ROOT_DIR, "MOT17testbench/data/HB/HB03/img1")
+
+# ## Configurations
+# 
+# We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
+# 
+# For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
+
+# In[2]:
+
 
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
+    ##BACKBONE = "resnet50"
 
 config = InferenceConfig()
 config.display()
+
+# ## Create Model and Load Trained Weights
+
+# In[3]:
+
 
 # Create model object in inference mode.
 model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
@@ -64,9 +75,32 @@ model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 # Load weights trained on MS-COCO
 model.load_weights(COCO_MODEL_PATH, by_name=True)
 
+# ## Class Names
+# 
+# The model classifies objects and returns class IDs, which are integer value that identify each class. Some datasets assign integer values to their classes and some don't. For example, in the MS-COCO dataset, the 'person' class is 1 and 'teddy bear' is 88. The IDs are often sequential, but not always. The COCO dataset, for example, has classes associated with class IDs 70 and 72, but not 71.
+# 
+# To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
+# 
+# To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
+# ```
+# # Load COCO dataset
+# dataset = coco.CocoDataset()
+# dataset.load_coco(COCO_DIR, "train")
+# dataset.prepare()
+# 
+# # Print class names
+# print(dataset.class_names)
+# ```
+# 
+# We don't want to require you to download the COCO dataset just to run this demo, so we're including the list of class names below. The index of the class name in the list represent its ID (first class is 0, second is 1, third is 2, ...etc.)
+
+# In[4]:
+
+
 # COCO Class names
 # Index of the class in the list is its ID. For example, to get ID of
 # the teddy bear class, use: class_names.index('teddy bear')
+'''
 class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'bus', 'train', 'truck', 'boat', 'traffic light',
                'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
@@ -82,23 +116,79 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
                'teddy bear', 'hair drier', 'toothbrush']
+'''
+class_names = ['BG', 'person']
+
+# ## Run Object Detection
+
+# In[7]:
+
 
 # Load a random image from the images folder
-file_names = next(os.walk(IMAGE_DIR))[2]
-image = skimage.io.imread(os.path.join(IMAGE_DIR, '1045023827_4ec3e8ba5c_z.jpg'))
+# file_names = next(os.walk(IMAGE_DIR))[2]
+# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+#f = open("test_output.txt","w")
+f2 = open("TestOutput/HB03_output.txt","w")
 
-# Run detection
-results = model.detect([image], verbose=1)
+frame = 0
 
-# Visualize results
-r = results[0]
-visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                            class_names, r['scores'])
+limit = 0
 
-image = skimage.io.imread(os.path.join(IMAGE_DIR,image_name))
-results = model.detect([image], verbose=1)
-r = results[0]
+lst = os.listdir(IMAGE_DIR)
+lst.sort()
 
-#visualize.save_image(image, image_name, r['rois'], r['masks'],
-#    r['class_ids'],r['scores'],coco_class_names,
-#    filter_classs_names=['bottle', 'wine glass'],scores_thresh=0.9,mode=0)
+images = []
+
+for dir in lst:
+    image = skimage.io.imread(os.path.join(IMAGE_DIR, dir))
+    images.append(image)
+
+##results = model.detect(images, verbose=1)  
+    
+##'''
+for image in images:
+    t1 = time.time()
+    results = model.detect([image], verbose=1)
+    r = results[0]
+    ##visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+    frame = frame + 1
+    for i in range(r['scores'].size):
+        print(r['class_ids'][i])
+        if r['class_ids'][i] == 1 :
+            ##f2 x1,y1,w,h
+            f2.write(str(frame)+",")
+            f2.write("-1,")
+            r['rois'][i][1].tofile(f2,",","%d")
+            f2.write(",")
+            r['rois'][i][0].tofile(f2,",","%d")
+            f2.write(",")
+            f2.write(str(r['rois'][i][3]-r['rois'][i][1]))
+            f2.write(",")
+            f2.write(str(r['rois'][i][2]-r['rois'][i][0]))
+            f2.write(",")
+            r['scores'][i].tofile(f2,",","%.7f")
+            f2.write(",")
+            f2.write("-1,-1,-1")
+            f2.write("\n")
+    print('Detection time is {}'.format(time.time() - t1))
+###'''
+
+#f.close()
+f2.close()
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
